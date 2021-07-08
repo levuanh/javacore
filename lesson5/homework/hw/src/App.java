170f51a7399bee30850ca14a22f47f4a6bcfb200
levuanh <levuanh1992@gmail.com>
2021-07-08 18:31:56 +0700

    Add files via upload

---
+++ lesson5/homework/hw/src/App.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+ class App{  
+ public static void main(String args[]){  
+  Student s1=new Student("A","Hang Bai",22);  
+  Student s2=new Student("B","Hoan Kiem",23);  
+  Student s2=new Student("C","Hai Ba Trung",24);  
+
+  ArrayList<Student> arl=new ArrayList<Student>();  
+  arl.add(s1);
+  arl.add(s2);  
+  arl.add(s3);  
+
+
+ }  
+}  
---
+++ lesson5/homework/hw/src/Student.java
@@ -0,0 +1,11 @@
+class Student{  
+  String name;  
+  String adrress;  
+  int age;  
+  
+  Student(String name,String adrress,int age){  
+   this.name=name;  
+   this.adrress=adrress;  
+   this.age=age;  
+  }  
+}  
